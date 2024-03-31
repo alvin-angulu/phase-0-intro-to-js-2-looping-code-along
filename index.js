3e2c718b2f1a01a93a84f70f30b14542b0025dad
Alvin Angulu <angulualvin@gmail.com>
2024-03-31 11:55:33 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+const friends = ["allen", "lucy", "david"];
+
+function writeCards(friends, event){
+    const updFriends = [];
+    for(let i = 0; i<friends.length; i++){
+
+     let message = `Thank you, ${friends[i]}, for the wonderful ${event} gift!`;
+     updFriends.push(message); 
+       
+    }
+    return updFriends;
+}
+//writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+//writeCards(friends, "wedding");
+
+
+function countDown(postiveInteger){
+    while(postiveInteger>=0){
+        console.log(postiveInteger);
+        postiveInteger--;
+    }
+
+}
+
+countDown(12);
